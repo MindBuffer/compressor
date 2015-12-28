f83e9253457c5e406070ed5c5af8044f01919c4f
mitchmindtree <mitchell.nordine@gmail.com>
2015-12-28 17:56:42 +1100

    Remove sound_stream in example in favour of using new portaudio api directly

--- examples/test.rs
+++ examples/test.rs
@@ -3,11 +3,16 @@
 extern crate compressor;
 extern crate dsp;
 extern crate envelope_detector;
+extern crate portaudio as pa;
 
 use compressor::{Compressor, Minimum};
-use dsp::{CallbackFlags, CallbackResult, Node, Settings, SoundStream, StreamParams};
+use dsp::Node;
 
 fn main() {
+    run().unwrap()
+}
+
+fn run() -> Result<(), pa::Error> {
 
     const CHANNELS: u16 = 2;
     const SAMPLE_HZ: f64 = 44_100.0;
@@ -21,39 +26,37 @@ fn main() {
         Compressor::<Minimum>::peak(ATTACK_MS, RELEASE_MS, SAMPLE_HZ, n_channels, THRESHOLD, RATIO);
 
     // Callback used to construct the duplex sound stream.
-    let callback = Box::new(move |input: &[f32], _in_settings: Settings,
-                                  output: &mut[f32], out_settings: Settings,
-                                  _dt: f64,
-                                  _: CallbackFlags| {
+    let callback = move |pa::DuplexStreamCallbackArgs { in_buffer, out_buffer, frames, .. }| {
 
         // Write the input to the output for fun.
-        for (out_sample, in_sample) in output.iter_mut().zip(input.iter()) {
+        for (out_sample, in_sample) in out_buffer.iter_mut().zip(in_buffer.iter()) {
             *out_sample = *in_sample;
         }
 
         println!("");
-        println!("{:?}", &output[0..4]);
+        println!("{:?}", &out_buffer[0..4]);
 
         // Update our rms state.
-        compressor.audio_requested(output, out_settings);
+        let settings = dsp::Settings::new(SAMPLE_HZ as u32, frames as u16, CHANNELS as u16);
+        compressor.audio_requested(out_buffer, settings);
 
-        println!("{:?}", &output[0..4]);
+        println!("{:?}", &out_buffer[0..4]);
 
-        CallbackResult::Continue
-    });
+        pa::Continue
+    };
 
-    // Construct parameters for a duplex stream and the stream itself.
-    let params = StreamParams::new().channels(CHANNELS as i32);
-    let stream = SoundStream::new()
-        .sample_hz(44_100.0)
-        .frames_per_buffer(128)
-        .duplex(params, params)
-        .run_callback(callback)
-        .unwrap();
+    // Construct PortAudio and the stream.
+    const FRAMES: u32 = 128;
+    let pa = try!(pa::PortAudio::new());
+    let chans = CHANNELS as i32;
+    let settings = try!(pa.default_duplex_stream_settings::<f32, f32>(chans, chans, SAMPLE_HZ, FRAMES));
+    let mut stream = try!(pa.open_non_blocking_stream(settings, callback));
+    try!(stream.start());
 
     // Wait for our stream to finish.
     while let Ok(true) = stream.is_active() {
         ::std::thread::sleep_ms(16);
     }
 
+    Ok(())
 }
